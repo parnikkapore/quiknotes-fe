a4c8382a7d1b26e580a455fb034a0b9c6fabc51a
Non Kittilakkananon <tata32000@hotmail.com>
2022-06-01 22:22:23 +0700

    Change sign in method to redirect

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import React, { useState, useEffect, useContext, createContext } from "react";
 import { config as firebaseConfig } from "../config/firebaseConfig.js";
@@ -77,7 +77,7 @@ function useProvideAuth() {
   };
 
   const signInWithGoogle = () => {
-    return signInWithPopup(firebaseAuth, googleAuthProvider);
+    return signInWithRedirect(firebaseAuth, googleAuthProvider);
   };
 
   // Subscribe to user on mount
