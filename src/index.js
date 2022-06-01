1b0a7e4790638dbb133f0a96e9495c68d481ef7f
Non Kittilakkananon <tata32000@hotmail.com>
2022-06-01 12:49:49 +0700

    Update root API for React 18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ProvideAuth } from "./hooks/useAuth";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <StrictMode>
     <CssBaseline />
     <ProvideAuth>
       <App />
     </ProvideAuth>
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
