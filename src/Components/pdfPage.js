6ce9596bbfa763555df92b696f956008d9e7dd67
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-06-02 21:35:54 +0700

    Promise chaining for pdfPage

--- src/Components/pdfPage.js
+++ src/Components/pdfPage.js
@@ -11,57 +11,67 @@ const pdfTemplate = {
   width: 0,
   height: 0,
   render() {
-    return <Image x={this.xpos} y={this.ypos} image={this.image} />;
+    return this.image ? (
+      <Image x={this.xpos} y={this.ypos} image={this.image} />
+    ) : (
+      <></>
+    );
   },
 };
 
 export const emptyPDF = pdfTemplate;
 
-export function addPDFAsync(url, setDoc, xpos = 0, ypos = 0) {
-  const result = Object.assign(Object.create(pdfTemplate), { xpos, ypos });
-  const loadingTask = pdfjs.getDocument(url);
-  loadingTask.promise.then(
-    (pdf) => {
-      console.log("PDF loaded");
+export async function addPDFAsync(url, setDoc, xpos = 0, ypos = 0) {
+  const docP = pdfjs.getDocument(url).promise;
+  const pageP = docP.then((pdf) => {
+    console.log("PDF loaded");
 
-      // Fetch the first page
-      const pageNumber = 1;
-      pdf.getPage(pageNumber).then((page) => {
-        console.log("Page loaded");
+    // Fetch the first page
+    const pageNumber = 1;
+    return pdf.getPage(pageNumber);
+  });
+  const renderedPageP = pageP.then(async (page) => {
+    console.log("Page loaded");
 
-        const scale = 1.5;
-        const viewport = page.getViewport({ scale: scale });
+    const scale = 1.0;
+    const viewport = page.getViewport({ scale: scale });
 
-        // Prepare canvas using PDF page dimensions
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext("2d");
-        result.width = viewport.width;
-        result.height = viewport.height;
-        canvas.height = result.height;
-        canvas.width = result.width;
+    // Prepare canvas using PDF page dimensions
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
 
-        // Render PDF page into canvas context
-        const renderContext = {
-          canvasContext: context,
-          viewport: viewport,
-        };
-        const renderTask = page.render(renderContext);
-        renderTask.promise.then(() => {
-          console.log("Page rendered");
-          result.image = canvas;
-          setDoc(result);
-        });
-      });
-    },
-    function (reason) {
-      // PDF loading error
-      console.error(reason);
-    }
-  );
+    // Render PDF page into canvas context
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    await page.render(renderContext).promise;
+
+    return canvas;
+  });
+  try {
+    const canvas = await renderedPageP;
+    console.log("Page rendered");
+    const result = Object.assign(Object.create(pdfTemplate), {
+      xpos,
+      ypos,
+      width: canvas.width,
+      height: canvas.height,
+      image: canvas,
+    });
+    setDoc(result);
+  } catch (e) {
+    // PDF loading error
+    console.error(e);
+  }
 }
 
 export function usePDFRenderer(pdfURL) {
   const [pdfDoc, setPDFDoc] = React.useState(pdfTemplate);
-  React.useEffect(() => addPDFAsync(pdfURL, setPDFDoc), [pdfURL]);
+  React.useEffect(() => {
+    addPDFAsync(pdfURL, setPDFDoc);
+  }, [pdfURL]);
   return pdfDoc;
 }
