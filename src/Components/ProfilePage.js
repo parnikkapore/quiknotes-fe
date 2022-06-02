385a8b7e2192de3df8f36660706fc4d1a9779cfe
Non Kittilakkananon <tata32000@hotmail.com>
2022-06-02 23:53:48 +0700

    Improve UI from MUI template

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { AppBar, Toolbar, Typography, IconButton, MenuItem, Menu } from "@mui/material";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 function ProfilePage() {
     const navigate = useNavigate();
@@ -9,13 +14,33 @@ function ProfilePage() {
     const handleNavigate = () => {
         navigate("/");
     }
-    navigate("/Profile");
+
     return (<>
-        <header>
-            <h1>Profile Page</h1>
-            <OverviewBox />
-        </header>
-        <Button onClick={handleNavigate}>Back</Button>
+    <AppShell />
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Profile
+                    </Typography>
+                    <OverviewBox />
+                    <Button
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={handleNavigate}
+                        >
+                            Back
+                        </Button>
+                    </Box>
+    </Container>
         </>
     );
 }
@@ -54,3 +79,75 @@ function OverviewBox(props) {
 }
 
 export default ProfilePage;
+
+function AppShell() {
+    const { user, signout } = useAuth();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const navigate = useNavigate();
+  
+    const handleLogout = () => {
+      handleClose();
+      signout();
+      navigate("/");
+    };
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const handleProfile = () => {
+      handleClose();
+      navigate("/");
+    }
+  
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            style={{ flexGrow: 1, textAlign: "left" }}
+          >
+            Creative Noters
+          </Typography>
+          {user && (
+            <div>
+              <IconButton
+                size="medium"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleProfile}>Canvas</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleLogout}>Log out</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    );
+  }
