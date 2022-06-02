7cdf91d05ca99bbaabcd37ad409aa31748b252c2
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-06-02 22:55:55 +0700

    Change hardcoded first item to map()
    I think you can tell where this is going

--- src/Hooks/useDocument/index.js
+++ src/Hooks/useDocument/index.js
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function useDocument(docInfo) {
   const [doc, setDoc] = React.useState(emptyPDF);
-  
+
   function onUpdate(docInfo) {
     addPDFAsync(docInfo.url, setDoc, docInfo.name);
   }
-  
+
   React.useEffect(() => {
     onUpdate(docInfo);
   }, [docInfo]);
-  
+
   return doc;
 }
