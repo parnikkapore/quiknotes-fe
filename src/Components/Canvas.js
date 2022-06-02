3de314e1eb18bb9272fae3eac848fc78efcb11c7
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-06-02 22:56:18 +0700

    prettier

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -116,7 +116,10 @@ export default function Canvas(props) {
 
   // === File opening and saving =====
 
-  const [docInfo, setDocInfo] = React.useState({name: "Test PDF", url: "/test1.pdf"});
+  const [docInfo, setDocInfo] = React.useState({
+    name: "Test PDF",
+    url: "/test1.pdf",
+  });
   const doc = useDocument(docInfo);
 
   function handlePDFOpen(e) {
@@ -128,7 +131,7 @@ export default function Canvas(props) {
       historyStep = 0;
       setLines([]);
 
-      setDocInfo({name: "Uploaded", url: e.target.result});
+      setDocInfo({ name: "Uploaded", url: e.target.result });
     };
     reader.readAsArrayBuffer(file);
   }
