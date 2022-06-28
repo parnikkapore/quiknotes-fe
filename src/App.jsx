357045ae70d5885d9909a1c906447ee6e030c357
Non Kittilakkananon <tata32000@hotmail.com>
2022-06-28 15:36:20 +0700

    Change file extension to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Canvas from "./Components/Canvas.js";
+import Canvas from "./Components/Canvas.jsx";
 import PageLogin from "./Components/PageLogin";
 import { useAuth } from "./hooks/useAuth";
 import AppShell from "./Components/AppShell";
