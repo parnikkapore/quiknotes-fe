1a3baaac79433e569947ac3f9e5f974370601681
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-07-09 11:35:02 +0700

    Moar tests

--- cypress/e2e/canvas.cy.js
+++ cypress/e2e/canvas.cy.js
@@ -38,7 +38,7 @@ function strokeLine1() {
     .trigger("mousemove", "right")
     .trigger("mousemove", "bottom")
     .trigger("mousemove", 100, 150)
-    .trigger("mouseup", 100, 150)
+    .trigger("mouseup", "top")
     .trigger("mousemove", 250, 250);
 }
 
@@ -67,6 +67,28 @@ function strokeLine4() {
     .trigger("mouseup", "right");
 }
 
+// This line overlaps significantly with lines 1 and 2 - good for draw mode testing
+function strokeLine5() {
+  cy.get("@konva")
+    .trigger("mousedown", 100, 200)
+    .trigger("mousemove", 500, 500)
+    .trigger("mousemove", 980, 400)
+    .trigger("mouseup", 980, 400);
+}
+
+// This line is a square around the center of the image, drawn in two segments
+function strokeLine6() {
+  cy.get("@konva")
+    .trigger("mousedown", 100, 100)
+    .trigger("mousemove", 900, 100)
+    .trigger("mousemove", 900, 400)
+    .trigger("mouseup", 900, 400)
+    .trigger("mousedown", 900, 400)
+    .trigger("mousemove", 100, 400)
+    .trigger("mousemove", 100, 100)
+    .trigger("mouseup", 100, 100);
+}
+
 function pickColor(color) {
   cy.get('[aria-label="Stroke color"]').click();
   cy.get(`[title='${color}']`).click();
@@ -82,9 +104,23 @@ function drawSampleLines() {
   strokeLine2();
 }
 
-function initSampleDocument() {
+// A zoomed-in view that makes it way easier to visual diff
+function initDrawingTestView() {
+  cy.contains("Reset view").click();
+  cy.get("@konva").trigger("wheel", "top", {
+    deltaX: 0,
+    deltaY: -1500,
+    ctrlKey: true,
+  });
+}
+
+function initDrawingTestState(doDrawSampleLines = false) {
   openFile("@testpdf");
-  drawSampleLines();
+  // cy.get('[aria-label="Pen"]').click(); // Make sure pen tool is activated
+  cy.get(".MuiSlider-colorPrimary").click("center"); // Enlarge the pen to make tests easier
+  initDrawingTestView(); // And make it even easier by zooming in
+
+  if (doDrawSampleLines) drawSampleLines();
 }
 
 function snap() {
@@ -175,15 +211,7 @@ describe("Viewport manipulation", () => {
 
 describe("Drawing state tracking", () => {
   beforeEach(() => {
-    openFile("@testpdf");
-    // cy.get('[aria-label="Pen"]').click(); // Make sure pen tool is activated
-    cy.get(".MuiSlider-colorPrimary").click("center"); // Enlarge the pen to make tests easier
-    cy.get("@konva").trigger("wheel", "top", {
-      // And make it even easier by zooming in
-      deltaX: 0,
-      deltaY: -1500,
-      ctrlKey: true,
-    });
+    initDrawingTestState(false);
   });
 
   it("Even works", () => {
@@ -196,7 +224,7 @@ describe("Drawing state tracking", () => {
     snap();
   });
 
-  it.only("Handles taps", () => {
+  it("Handles taps", () => {
     cy.get("@konva").trigger("wheel", "top", {
       // This crud only works zoomed in to the max
       deltaX: 0,
@@ -208,6 +236,59 @@ describe("Drawing state tracking", () => {
       .trigger("mousemove", "center")
       .trigger("mouseup", "center");
     snap();
+  });
+
+  it("Can draw beyond top and left", () => {
+    // scroll to top left corner
+    cy.get("@konva").trigger("wheel", "top", {
+      deltaX: -3500,
+      deltaY: -200,
+    });
+
+    strokeLine6();
+    snap();
+  });
+
+  it("Can draw beyond bottom and right", () => {
+    // scroll to bottom right corner
+    cy.get("@konva").trigger("wheel", "top", {
+      deltaX: 3500,
+      deltaY: 10300,
+    });
+
+    strokeLine6();
+    snap();
+  });
+
+  it("Can draw on second page", () => {
+    // scroll to second page
+    cy.get("@konva").trigger("wheel", "top", {
+      deltaX: 0,
+      deltaY: 11000,
+    });
+
+    strokeLine1();
+    snap();
+  });
+
+  it("Does something reasonable on MUDMU", () => {
+    cy.get("@konva")
+      .trigger("mousemove", "center")
+      .trigger("mousemove", "bottomRight")
+      .trigger("mouseup", "bottomRight")
+      .trigger("mousedown", "bottomLeft")
+      .trigger("mousemove", "topLeft")
+      .trigger("mousemove", "right")
+      .trigger("mouseup", "right");
+  });
+
+  it("Does something reasonable on UDMU", () => {
+    cy.get("@konva")
+      .trigger("mouseup", "bottomRight")
+      .trigger("mousedown", "bottomLeft")
+      .trigger("mousemove", "topLeft")
+      .trigger("mousemove", "right")
+      .trigger("mouseup", "right");
   });
 
   it("Does something reasonable on DDMMUU", () => {
@@ -249,15 +330,7 @@ describe("Drawing state tracking", () => {
 
 describe("Drawing settings", () => {
   beforeEach(() => {
-    openFile("@testpdf");
-    // cy.get('[aria-label="Pen"]').click(); // Make sure pen tool is activated
-    cy.get(".MuiSlider-colorPrimary").click("center"); // Enlarge the pen to make tests easier
-    cy.get("@konva").trigger("wheel", "top", {
-      // And make it even easier by zooming in
-      deltaX: 0,
-      deltaY: -1500,
-      ctrlKey: true,
-    });
+    initDrawingTestState(false);
   });
 
   it("can adjust line color", () => {
@@ -278,5 +351,165 @@ describe("Drawing settings", () => {
     cy.get(".MuiSlider-colorPrimary").click("right");
     strokeLine3();
     snap();
+  });
+});
+
+describe("Drawing tools", () => {
+  beforeEach(() => {
+    initDrawingTestState(true);
+  });
+
+  it("Line tool", () => {
+    strokeLine3();
+    snap();
+  });
+
+  it("Eraser tool", () => {
+    cy.get('[aria-label="Eraser"]').click();
+    strokeLine5();
+    snap();
+  });
+
+  it("Highlighter tool", () => {
+    cy.get('[aria-label="Highlighter"]').click();
+    strokeLine5();
+    snap();
+  });
+});
+
+describe("Undo and redo", () => {
+  beforeEach(() => {
+    initDrawingTestState(true);
+  });
+
+  it("works on line draws", () => {
+    cy.get('[aria-label="Undo"]').click();
+    snap();
+    cy.get('[aria-label="Redo"]').click();
+    snap();
+  });
+
+  it("updates properly when a new line is drawn", () => {
+    // Draw a new line (so we have 3 states)
+    pickColor("#50E3C2");
+    strokeLine3();
+
+    // Undo twice
+    cy.get('[aria-label="Undo"]').click();
+    cy.get('[aria-label="Undo"]').click();
+
+    // Stroke a new line, clearing the two redo states
+    pickColor("#9013FE");
+    strokeLine4();
+
+    // 1: Redo does nothing
+    cy.get('[aria-label="Redo"]').click();
+    snap();
+
+    // 2: Undoing and redoing brings you back to the same state
+    cy.get('[aria-label="Undo"]').click();
+    cy.get('[aria-label="Redo"]').click();
+    snap();
+  });
+
+  // TODO: The following test is currently expected NOT to work.
+  it.skip("works on clear screen", () => {
+    cy.contains("Clear").click();
+    cy.get('[aria-label="Undo"]').click();
+    snap();
+    cy.get('[aria-label="Redo"]').click();
+    snap();
+  });
+});
+
+describe("Clear screen", () => {
+  it("works", () => {
+    initDrawingTestState(true);
+    cy.contains("Clear").click();
+    snap();
+  });
+});
+
+describe("Add new page", () => {
+  beforeEach(() => {
+    openFile("@testpdf");
+  });
+
+  it("works", () => {
+    cy.get("@konva").click(720, 520);
+    cy.get("@konva").trigger("wheel", "top", {
+      deltaX: 0,
+      deltaY: 400,
+      ctrlKey: true,
+    });
+    snap();
+  });
+
+  it("Correctly shifts lines drawn on different pages", () => {
+    initDrawingTestView();
+    cy.get("@konva").trigger("wheel", "center", {
+      deltaX: 0,
+      deltaY: 10300,
+    });
+
+    // cy.get('[aria-label="Pen"]').click(); // Make sure pen tool is activated
+    cy.get(".MuiSlider-colorPrimary").click("center"); // Enlarge the pen to make tests easier
+
+    // A: A line drawn on the bottom
+    pickColor("#000000");
+    cy.get("@konva")
+      .trigger("mousedown", "bottomLeft")
+      .trigger("mousemove", 500, 400)
+      .trigger("mousemove", "bottomRight")
+      .trigger("mouseup", "bottomRight");
+
+    // B: A line drawn on the top
+    pickColor("#F5A623");
+    cy.get("@konva")
+      .trigger("mousedown", "topLeft")
+      .trigger("mousemove", 500, 50)
+      .trigger("mousemove", "topRight")
+      .trigger("mouseup", "topRight");
+
+    // C: A line drawn through the entire image, biasing top
+    cy.get("@konva").trigger("wheel", "center", {
+      deltaX: 0,
+      deltaY: -50,
+    });
+    pickColor("#50E3C2");
+    strokeLine2();
+
+    // D: A line drawn through the entire image, biasing bottom
+    cy.get("@konva").trigger("wheel", "center", {
+      deltaX: 0,
+      deltaY: 100,
+    });
+    pickColor("#9013FE");
+    strokeLine1();
+
+    // Now open a new page...
+    cy.contains("Reset view").click();
+    cy.get("@konva").click(720, 520);
+
+    // 1: Lines B, C stay with the top page
+    initDrawingTestView();
+    cy.get("@konva").trigger("wheel", "center", {
+      deltaX: 0,
+      deltaY: 10300,
+    });
+    snap();
+
+    // 2: Lines A, D move down with the bottom page
+    cy.get("@konva").trigger("wheel", "center", {
+      deltaX: 0,
+      deltaY: 10800,
+    });
+    snap();
+  });
+});
+
+describe("Export", () => {
+  beforeEach(() => {
+    initDrawingTestState(true);
   });
 });
