263645956b0e33020011fdc5f8ef0eb8f30683ba
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-07-19 22:05:14 +0700

    Remove unused test template

--- cypress/e2e/smoketest.cy.js
+++ cypress/e2e/smoketest.cy.js
@@ -24,29 +24,6 @@ describe("Drawing canvas", () => {
       .wait(1000); // Hack to wait until PDF renders
   });
 
-  /*
-  it("actually renders", () => {
-    cy.document().toMatchImageSnapshot();
-  });
-  
-  it("scrolls horizontally and vertically", () => {
-    cy.get("@konva").trigger("wheel", { deltaX: 100, deltaY: 250 });
-    cy.get("@konva").wait(10).toMatchImageSnapshot();
-  });
-
-  it("can be drawn on with a mouse", () => {
-    cy.get('[aria-label="Pen"]').click(); // Make sure pen tool is activated
-    cy.get(".MuiSlider-colorPrimary").click("center"); // Enlarge the pen to make tests easier
-    cy.get("@konva")
-      .trigger("mousemove", 80, 50)
-      .trigger("mousedown", 100, 150)
-      .trigger("mousemove", "center")
-      .trigger("mouseup", 150, 100)
-      .trigger("mousemove", "bottomLeft");
-    cy.get("@konva").wait(10).toMatchImageSnapshot();
-  });
-  */
-
   it("at least works as intended", () => {
     cy.log("Opens a PDF");
     cy.fixture("test_media/pdf.pdf", { encoding: null }).as("testpdf");
