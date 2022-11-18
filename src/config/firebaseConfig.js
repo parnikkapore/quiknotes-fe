79b4c9034a8657044ad7ade892fee51b531a0594
Non Kittilakkananon <tata32000@hotmail.com>
2022-11-18 22:03:12 +0800

    add .env file for apiKey

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,9 @@
 // Firebase Config
 export const config = {
-  apiKey: "AIzaSyBexBgkw5s1YqBCKzy1Ubk8q07Ll2EbPYA",
-  authDomain: "creative-noters.firebaseapp.com",
-  projectId: "creative-noters",
-  storageBucket: "creative-noters.appspot.com",
-  messagingSenderId: "42713012892",
-  appId: "1:42713012892:web:5a654cf159c602b6f22b59",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
